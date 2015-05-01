e0f8ac054d4b3acc9e7f61d6a4bc979161c6f3ad
Jake Martin <jmart.alb@gmail.com>
2015-05-01 17:17:58 -0400

    basic multithreaded server functionality

--- client.py
+++ client.py
@@ -1,8 +1,14 @@
-import sys, copy, multiprocessing, socket
+import sys, copy, multiprocessing, socket,time
 
 class Client:
     def __init__ (self):
-        self.clientsConnected = 0
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(("localhost", 9000))
+        data = "some data"
+        sock.sendall(data)
 
+        result = sock.recv(1024)
+        print result
+        sock.close()
 if __name__ == '__main__':
     Client()
--- server.py
+++ server.py
@@ -1,23 +1,49 @@
-import sys, copy, multiprocessing, SocketServer
+import sys, copy, multiprocessing, socket
 from client import Client
+def handleRequest(connection, address):
+        while True:
+            data = connection.recv(1024)
+            if data == "":
+                print"Socket closed remotely"
+                break
+            print "Received data" + data
+            connection.sendall("ack")
 
-class RequestHandler(SocketServer.BaseRequestHandler):
-    def __init__ (self):
+
+class Server():
+    def __init__ (self,ip, port):
         self.socketsOpen = 0
+        self.ip = ip
+        self.port = port
+    def start(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.ip, self.port))
+        self.socket.listen(1)
+
+        while True:
+            conn, address = self.socket.accept()
+            process = multiprocessing.Process(target=handleRequest, args=(conn, address))
+            process.daemon = True
+            process.start()
 
 
-    def receive_message(self):
-        #delimit by /n
-        return
-if __name__ == '__main__':
-    HOST, PORT = "localhost", 9999
-    print "Listening on port" + str(PORT)
-    server = SocketServer.TCPServer((HOST, PORT), RequestHandler)
+
+
+
+if __name__ == "__main__":
+    HOST, PORT = "0.0.0.0", 9000
+    #Client(HOST,PORT);
+    server = Server(HOST, PORT)
+    try:
+        print "Listening on port " + str(PORT)
+        server.start()
+    except:
+        logging.exception("Unexpected exception")
+    finally:
+        print "Shutting down"
+        for process in multiprocessing.active_children():
+            process.terminate()
+            process.join()
 
 
     #instantiate the client
-    Client();
-
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
