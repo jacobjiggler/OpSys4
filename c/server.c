beb0abd90d7532142bf249960365afc7cd3cb397
Jake Martin <jmart.alb@gmail.com>
2015-05-05 16:15:32 -0400

    pseudocoded out the parser, and created the page struct

--- c/server.c
+++ c/server.c
@@ -20,15 +20,87 @@
 #define FRAMES = 32
 #define FRAMES_PER_FILE 4
 
+//declare mutex locks up here
+
+//struct to hold page info
+struct page {
+  char filename[1000];
+  int pageNum;
+
+  //Will hold the number in which it was edited
+  int lastEdited;
+};
+
 void *connection_handler(void *socket_desc)
 {
+  //(PARSE OUT COMMAND OR RETURN ERROR)
+    //if !string compare(client_message(first 4 char) "DIR\n")
+      //call dir function
+
+
+    //elseif !string compare(client_message(first 6 char) "STORE ")
+      //if \n in client message
+        //save index of \n
+      //else
+        //printf("ERROR: Incorrect Syntax For COMMAND\n");
+        //return
+      //if there is a digit before \n
+        //store index of first digit in string of digits
+        //convert num to int and save as bytes
+      //else
+        //printf("ERROR: Incorrect Syntax For COMMAND\n");
+        //return
+      //save filename(dont forget about the space between filename and bytes)
+      //either create and call a store function or write it here
+
+
+    //else if !string compare(client_message(first 5 char) "READ ")
+      //if \n in client message
+        //save index of \n
+      //else
+        //printf("ERROR: Incorrect Syntax For COMMAND\n");
+        //return
+      //if there is a digit before \n
+        //store index of first digit in string of digits
+        //convert num to int and save as length
+      //else
+        //printf("ERROR: Incorrect Syntax For COMMAND\n");
+        //return
+      //if there is a digit before index of previous digits
+        //store index of first digit in string of digits
+        //convert num to int and save as byteoffset
+      //else
+        //printf("ERROR: Incorrect Syntax For COMMAND\n");
+        //return
+      //save filename (dont foget the space)
+      //either create and call a READ function or write it here
+
+
+    //else if !string compare(client_message(first 5 char) "DELETE ")
+      //if \n in client message
+        //save index of \n
+      //else
+        //printf("ERROR: Incorrect Syntax For COMMAND\n");
+        //return
+      //if there is a char before \n
+        //save filename
+      //else
+        //printf("ERROR: Incorrect Syntax For COMMAND\n");
+        //return
+      //call delete function
+
+
+    //else
+      //printf("ERROR: Incorrect Syntax For COMMAND\n");
+      //return
+
+
 
 
 
 
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
-    //printf("%d \n", sock);
     int read_size = 0;
     char client_message[2000];
 
@@ -68,6 +140,11 @@ void *connection_handler(void *socket_desc)
 
 int main(int argc , char *argv[])
 {
+  //declare memory array here(32 slots with 1024 bits)
+    // allocate 32,768 with calloc
+    //then turn into array of char of that size
+  //declare page table here (32 slots filled with custom struct page)
+
 	int socket_desc , client_sock, c;
 	struct sockaddr_in server, client;
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -94,6 +171,7 @@ int main(int argc , char *argv[])
 	pthread_t thread_id;
 	while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
 	{
+
 	    if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &client_sock) < 0)
         {
             perror("failed to create thread");
